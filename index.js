bd7c14e15a0c7835f39ff1e2f96634ccbd0133f0
ashley <ashley.oketo@student.moringaschool.com>
2022-08-23 17:08:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const findMatching = function(drivers, rider){
+    const result = drivers.filter((driver) => driver.toLowerCase() === rider.toLowerCase())
+    return result;
+};
+
+const fuzzyMatch = function(driverNames, rider){
+    const newMatch = driverNames.filter(function(driverName){
+        if(driverName.startsWith(rider)){
+            return true;
+        }else{
+            return false;
+        }
+    })
+    return newMatch;
+}
+const matchName = function(drivers, rider){
+    const myMatch = drivers.filter(function(driver){
+        return driver.name === rider;
+    });
+    return myMatch;
+}
